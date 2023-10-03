b4406201a100446ae95a0f67b60cb23e067e70c6
System Administrator <gn.dimas@gmail.com>
2023-10-03 15:40:18 +0700

    Connecting upload function to Amazon S3

--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -7,6 +7,26 @@ export async function uploadToS3(file: File) {
             secretAccessKey:process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY
         })
         
+        const s3 = new AWS.S3({
+            params: {
+                Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME,
+            },
+            region: 'ap-southeast-1',
+        })
+
+        const file_key ='uploads/' + Date.now().toString()+ file.name.replace(' ', '-');
+
+        const params = {
+            Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME!,
+            Key: file_key,
+            Body: file
+        }
+
+        const upload = s3.putObject(params).on('httpUploadProgress', evt => {
+            console.log(`uploading to s3...`, parseInt(((evt.loaded* 100) / evt.total).toString()))
+        })
+
+
     } catch (error) {
         console.error(`Upload to S3 error: ${error}`);
     }
